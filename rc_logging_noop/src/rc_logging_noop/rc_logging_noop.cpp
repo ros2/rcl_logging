9d04ee89c763b4b4cc635d193b516c52454d2717
burekn <burekn@amazon.com>
2018-12-04 22:15:40 -0800

    Adding a noop logger ext_lib and adding COLCON_IGNORE to the log4cxx until the windows/mac dependency can be resolved.

---
+++ rc_logging_noop/src/rc_logging_noop/rc_logging_noop.cpp
@@ -0,0 +1,46 @@
+// Copyright 2018 Open Source Robotics Foundation, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+
+extern "C" {
+
+typedef int rc_logging_ret_t;
+#define RC_LOGGING_RET_OK   (0)
+
+rc_logging_ret_t rcl_logging_external_initialize(const char * config_file)
+{
+  (void) config_file;
+  return RC_LOGGING_RET_OK;
+}
+
+rc_logging_ret_t rcl_logging_external_shutdown()
+{
+  return RC_LOGGING_RET_OK;
+}
+
+void rcl_logging_external_log(int severity, const char * name, const char * msg)
+{
+  (void) severity;
+  (void) name;
+  (void) msg;
+}
+
+rc_logging_ret_t rcl_logging_external_set_logger_level(const char * name, int level)
+{
+  (void) name;
+  (void) level;
+  return RC_LOGGING_RET_OK;
+}
+
+} /* extern "C" */
