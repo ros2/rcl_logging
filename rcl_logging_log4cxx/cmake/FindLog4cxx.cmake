a8220851d0c5f6f49dbc66e01b3f507451d886ea
Burek <burekn@amazon.com>
2018-12-18 15:51:28 -0800

    Fixing the case for finding log4cxx library.

--- rcl_logging_log4cxx/cmake/FindLog4cxx.cmake
+++ rcl_logging_log4cxx/cmake/FindLog4cxx.cmake
@@ -13,11 +13,11 @@
 if( Log4cxx_DIR )
   find_package( Log4cxx NO_MODULE )
 elseif( NOT Log4cxx_FOUND )
-  message(STATUS "Searching for Log4cxx/logger.h")
-  find_path( Log4cxx_INCLUDE_DIR Log4cxx/logger.h )
+  message(STATUS "Searching for log4cxx/logger.h")
+  find_path( Log4cxx_INCLUDE_DIR log4cxx/logger.h )
 
-  message(STATUS "Searching for libLog4cxx")
-  find_library( Log4cxx_LIBRARIES Log4cxx )
+  message(STATUS "Searching for liblog4cxx")
+  find_library( Log4cxx_LIBRARIES log4cxx )
 
   include( FindPackageHandleStandardArgs )
   FIND_PACKAGE_HANDLE_STANDARD_ARGS( Log4cxx Log4cxx_INCLUDE_DIR Log4cxx_LIBRARIES )
