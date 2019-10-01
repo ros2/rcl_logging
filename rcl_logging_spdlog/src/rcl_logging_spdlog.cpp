b32968fb165f5836e6868e3d47c0859c839a8f05
Chris Lalancette <clalancette@openrobotics.org>
2019-10-01 13:25:43 +0000

    Fixes from review.
    Signed-off-by: Chris Lalancette <clalancette@openrobotics.org>

--- rcl_logging_spdlog/src/rcl_logging_spdlog.cpp
+++ rcl_logging_spdlog/src/rcl_logging_spdlog.cpp
@@ -20,6 +20,7 @@
 #include <rcutils/allocator.h>
 #include <rcutils/filesystem.h>
 #include <rcutils/get_env.h>
+#include <rcutils/logging.h>
 #include <rcutils/process.h>
 #include <rcutils/snprintf.h>
 #include <rcutils/time.h>
@@ -29,8 +30,8 @@
 
 #include "rcl_logging_spdlog/logging_interface.h"
 
-#define RC_LOGGING_RET_OK                          (0)
-#define RC_LOGGING_RET_ERROR                       (2)
+#define RC_LOGGING_RET_OK    (0)
+#define RC_LOGGING_RET_ERROR (2)
 
 #ifdef __cplusplus
 extern "C" {
@@ -39,33 +40,20 @@ extern "C" {
 static std::mutex g_logger_mutex;
 static std::shared_ptr<spdlog::logger> g_root_logger = nullptr;
 
-/* These are defined here to match the severity levels in rcl. They provide a consistent way for external logger
-    implementations to map between the incoming integer severity from ROS to the concept of DEBUG, INFO, WARN, ERROR,
-    and FATAL*/
-enum RC_LOGGING_LOG_SEVERITY
-{
-  RC_LOGGING_SEVERITY_UNSET = 0,  ///< The unset log level
-  RC_LOGGING_SEVERITY_DEBUG = 10,  ///< The debug log level
-  RC_LOGGING_SEVERITY_INFO = 20,  ///< The info log level
-  RC_LOGGING_SEVERITY_WARN = 30,  ///< The warn log level
-  RC_LOGGING_SEVERITY_ERROR = 40,  ///< The error log level
-  RC_LOGGING_SEVERITY_FATAL = 50,  ///< The fatal log level
-};
-
 static spdlog::level::level_enum map_external_log_level_to_library_level(int external_level)
 {
   spdlog::level::level_enum level;
 
   // map to the next highest level of severity
-  if (external_level <= RC_LOGGING_SEVERITY_DEBUG) {
+  if (external_level <= RCUTILS_LOG_SEVERITY_DEBUG) {
     level = spdlog::level::level_enum::debug;
-  } else if (external_level <= RC_LOGGING_SEVERITY_INFO) {
+  } else if (external_level <= RCUTILS_LOG_SEVERITY_INFO) {
     level = spdlog::level::level_enum::info;
-  } else if (external_level <= RC_LOGGING_SEVERITY_WARN) {
+  } else if (external_level <= RCUTILS_LOG_SEVERITY_WARN) {
     level = spdlog::level::level_enum::warn;
-  } else if (external_level <= RC_LOGGING_SEVERITY_ERROR) {
+  } else if (external_level <= RCUTILS_LOG_SEVERITY_ERROR) {
     level = spdlog::level::level_enum::err;
-  } else if (external_level <= RC_LOGGING_SEVERITY_FATAL) {
+  } else if (external_level <= RCUTILS_LOG_SEVERITY_FATAL) {
     level = spdlog::level::level_enum::critical;
   }
   return level;
@@ -84,6 +72,7 @@ rcl_logging_ret_t rcl_logging_external_initialize(const char * config_file, rcut
   bool config_file_provided = (nullptr != config_file) && (config_file[0] != '\0');
   if (config_file_provided) {
     // TODO(clalancette): implement support for an external configuration file.
+    RCUTILS_SET_ERROR_MSG("spdlog logging backend doesn't currently support external configuration");
     return RC_LOGGING_RET_ERROR;
   } else {
     // To be compatible with ROS 1, we construct a default filename of
@@ -95,6 +84,7 @@ rcl_logging_ret_t rcl_logging_external_initialize(const char * config_file, rcut
       // We couldn't get the home directory; it is not really going to be
       // possible to do logging properly, so get out of here without setting
       // up logging.
+      RCUTILS_SET_ERROR_MSG("Failed to get users home directory");
       return RC_LOGGING_RET_ERROR;
     }
 
@@ -103,17 +93,21 @@ rcl_logging_ret_t rcl_logging_external_initialize(const char * config_file, rcut
     char name_buffer[4096] = {0};
     int print_ret = rcutils_snprintf(name_buffer, sizeof(name_buffer), "%s/.ros", homedir);
     if (print_ret < 0) {
+      RCUTILS_SET_ERROR_MSG("Failed to create home directory string");
       return RC_LOGGING_RET_ERROR;
     }
     if (!rcutils_mkdir(name_buffer)) {
+      RCUTILS_SET_ERROR_MSG("Failed to create user .ros directory");
       return RC_LOGGING_RET_ERROR;
     }
 
     print_ret = rcutils_snprintf(name_buffer, sizeof(name_buffer), "%s/.ros/log", homedir);
     if (print_ret < 0) {
+      RCUTILS_SET_ERROR_MSG("Failed to create log directory string");
       return RC_LOGGING_RET_ERROR;
     }
     if (!rcutils_mkdir(name_buffer)) {
+      RCUTILS_SET_ERROR_MSG("Failed to create user log directory");
       return RC_LOGGING_RET_ERROR;
     }
 
@@ -122,7 +116,8 @@ rcl_logging_ret_t rcl_logging_external_initialize(const char * config_file, rcut
     rcutils_ret_t ret = rcutils_system_time_now(&now);
     if (ret != RCUTILS_RET_OK) {
       // We couldn't get the system time, so get out of here without setting up
-      // logging.
+      // logging.  We don't need to call RCUTILS_SET_ERROR_MSG either since
+      // rcutils_system_time_now() already did it.
       return RC_LOGGING_RET_ERROR;
     }
     int64_t ms_since_epoch = RCUTILS_NS_TO_MS(now);
@@ -132,12 +127,14 @@ rcl_logging_ret_t rcl_logging_external_initialize(const char * config_file, rcut
     if (basec == NULL) {
       // We couldn't get the program name, so get out of here without setting up
       // logging.
+      RCUTILS_SET_ERROR_MSG("Failed to get the executable name");
       return RC_LOGGING_RET_ERROR;
     }
 
     print_ret = rcutils_snprintf(name_buffer, sizeof(name_buffer), "%s/.ros/log/%s_%i_%" PRId64 ".log", homedir, basec, rcutils_get_pid(), ms_since_epoch);
     allocator.deallocate(basec, allocator.state);
     if (print_ret < 0) {
+      RCUTILS_SET_ERROR_MSG("Failed to create log file name string");
       return RC_LOGGING_RET_ERROR;
     }
     g_root_logger = spdlog::basic_logger_mt("root", name_buffer);
